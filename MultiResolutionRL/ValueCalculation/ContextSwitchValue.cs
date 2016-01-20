7ce8721256b4ab15a0b92cb4c9e791d1b3f5013c
Eric <eric.chalmers@uleth.ca>
2016-01-20 16:13:25 -0700

    context switch based on state history

--- MultiResolutionRL/ValueCalculation/ContextSwitchValue.cs
+++ MultiResolutionRL/ValueCalculation/ContextSwitchValue.cs
@@ -17,12 +17,15 @@ namespace MultiResolutionRL.ValueCalculation
         List<ModelBasedValue<stateType, actionType>> models = new List<ModelBasedValue<stateType, actionType>>();
         ModelBasedValue<stateType, actionType> currentModel;
 
+        Queue<StateTransition<stateType, actionType>> transitionHistory = new Queue<StateTransition<stateType, actionType>>();
+
         public ContextSwitchValue(IEqualityComparer<stateType> StateComparer, IEqualityComparer<actionType> ActionComparer, List<actionType> AvailableActions, stateType StartState, params object[] parameters)
             : base(StateComparer, ActionComparer, AvailableActions, StartState, parameters)
         {
             stateComparer = StateComparer;
             actionComparer = ActionComparer;
             availableActions = AvailableActions;
+            startState = StartState;
             
             models.Add(new ModelBasedValue<stateType, actionType>(StateComparer, ActionComparer, availableActions, StartState, parameters));
             //models.Add(new ModelBasedValue<stateType, actionType>(StateComparer, ActionComparer, availableActions, StartState, parameters));
@@ -52,44 +55,44 @@ namespace MultiResolutionRL.ValueCalculation
 
         public override double update(StateTransition<stateType, actionType> transition)
         {
-            currentModel.update(transition);
+            transitionHistory.Enqueue(transition);
+            while (transitionHistory.Count() > 5)
+                transitionHistory.Dequeue();
+
+            //double[] ps = new double[models.Count];
+            //for (int i = 0; i < ps.Length; i++)
+            //{
+            //    ps[i] = Math.Round(Tprobability(transitionHistory, models[i]),2);
+            //}
+            //Console.WriteLine(string.Join(",", ps));
 
 
-            double[] ps = new double[models.Count];
-            for (int i=0; i<ps.Length; i++)
-            {
-                ps[i] = Tprobability(transition, models[i]);
-            }
-            Console.WriteLine(string.Join(",", ps));
-
-
-            double bestP = Tprobability(transition, currentModel);
-
+            // select model
+            double bestP = Tprobability(transitionHistory, currentModel);
             foreach (ModelBasedValue<stateType, actionType> m in models)
             {
                 if (m == currentModel)
                     continue;
 
-                double thisP = Tprobability(transition, m);
+                double thisP = Tprobability(transitionHistory, m);
 
                 if (thisP > bestP)
                 {
                     Console.WriteLine("switching to previously learned model: " + models.IndexOf(m));
-                    if (models.IndexOf(m)==2)
-                    {
-                        int a = 0;
-                    }
                     currentModel = m;
                     bestP = thisP;
                 }
             }
 
+            // create new model if necessary
             if (bestP < 0.5)
             {
                 Console.WriteLine("creating new model");
                 currentModel = new ModelBasedValue<stateType, actionType>(stateComparer, actionComparer, availableActions, startState);
                 models.Add(currentModel);
             }
+
+            currentModel.update(transition);
             
             return 0;
         }
@@ -99,18 +102,21 @@ namespace MultiResolutionRL.ValueCalculation
             return currentModel.value(state, actions);
         }
         
-        double Tprobability(StateTransition<stateType,actionType> transition, ModelBasedValue<stateType, actionType> model)
+        double Tprobability(IEnumerable<StateTransition<stateType, actionType>> transitions, ModelBasedValue<stateType, actionType> model)
         {
-            Dictionary<stateType, int> s2Counts = model.T.GetStateValueTable(transition.oldState, transition.action);
-            double thisS2Counts = 0;
-            if (s2Counts.ContainsKey(transition.newState))
-                thisS2Counts = (double)s2Counts[transition.newState];
-            double total = (double)s2Counts.Values.Sum();
-
-            if (total == 0)
-                return 0;
-            else
-                return thisS2Counts / total;
+            double p = 1;
+            foreach (StateTransition<stateType, actionType> transition in transitions)
+            {
+                Dictionary<stateType, int> s2Counts = model.T.GetStateValueTable(transition.oldState, transition.action);
+                double thisS2Counts = 1;
+                if (s2Counts.ContainsKey(transition.newState))
+                    thisS2Counts = (double)s2Counts[transition.newState] + 1;
+                double total = (double)s2Counts.Values.Sum() + 1;
+                
+                p *= (thisS2Counts / total);
+            }
+            return p;
         }
+        
     }
 }
