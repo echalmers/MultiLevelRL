6eba01a1333247b3f7df9072146eb7cff9bd06d9
robertsonb93 <robertsonb@uleth.ca>
2016-01-22 11:33:41 -0700

    Made a file for ModelBasedActionValue

--- MultiResolutionRL/ValueCalculation/LSValue.cs
+++ MultiResolutionRL/ValueCalculation/LSValue.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MultiResolutionRL.ValueCalculation;
 
 namespace MultiResolutionRL.ValueCalculation
 {
